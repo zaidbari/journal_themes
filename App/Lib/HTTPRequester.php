e451940af12b8a2ced40777d845b78676f1c511d
zaidbari <zaidbari9@hotmail.com>
2021-09-28 19:52:25 +0300

    Cache issue

--- App/Lib/HTTPRequester.php
+++ App/Lib/HTTPRequester.php
@@ -19,55 +19,55 @@ class HTTPRequester
 	 */
 	public static function HTTPGet( $url, $cache  = NULL, array $params = [], $headers = [],  $expires = NULL ) : array
 	{
-//		$query = http_build_query($params);
-//		$ch = curl_init($url . '?' . $query);
-//		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//		curl_setopt($ch, CURLOPT_HEADER, 1);
-//		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$query = http_build_query($params);
+		$ch = curl_init($url . '?' . $query);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+		$response = curl_exec($ch);
+		$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+		$header = substr($response, 0, $header_size);
+		$body = substr($response, $header_size);
+		$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		curl_close($ch);
+		return ["header" => $header, "body" => json_decode($body), "code" => $code];
+
+//		$path = $_SERVER['DOCUMENT_ROOT'] . '/api_cache/';
+//		if (!file_exists($path)) mkdir($path, 0777, true);
+//		$cache_file = $cache ? $path . $cache : $path . 'api-cache.json';
+//		if( !$expires ) $expires = time() - 1 * 60 * 60;
+//		if( !file_exists($cache_file) ) fopen( $cache_file, "w");
 //
-//		$response = curl_exec($ch);
-//		$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-//		$header = substr($response, 0, $header_size);
-//		$body = substr($response, $header_size);
-//		$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//		// Check that the file is older than the expiry time and that it's not empty
+//		if ( filectime($cache_file) < $expires || file_get_contents( $cache_file)  == '' ) {
 //
-//		curl_close($ch);
-//		return ["header" => $header, "body" => json_decode($body), "code" => $code];
-
-		$path = $_SERVER['DOCUMENT_ROOT'] . '/api_cache/';
-		if (!file_exists($path)) mkdir($path, 0777, true);
-		$cache_file = $cache ? $path . $cache : $path . 'api-cache.json';
-		if( !$expires ) $expires = time() - 1 * 60 * 60;
-		if( !file_exists($cache_file) ) fopen( $cache_file, "w");
-
-		// Check that the file is older than the expiry time and that it's not empty
-		if ( filectime($cache_file) < $expires || file_get_contents( $cache_file)  == '' ) {
-
-			// File is too old, refresh cache
-			$query = http_build_query($params);
-			$ch = curl_init($url . '?' . $query);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_HEADER, 1);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			$response = curl_exec($ch);
-			$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-			$header = substr($response, 0, $header_size);
-			$body = substr($response, $header_size);
-			$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-			curl_close($ch);
-
-			$api_results = ["header" => $header, "body" => json_decode($body), "code" => $code];
-			$json_results = json_encode($api_results);
-
-			// Remove cache file on error to avoid writing wrong xml
-			if ( $api_results && $json_results )
-				file_put_contents($cache_file, $json_results);
-			else
-				unlink( $cache_file);
-		} else {
-			$json_results = file_get_contents($cache_file);
-		}
-		return (array) json_decode($json_results);
+//			// File is too old, refresh cache
+//			$query = http_build_query($params);
+//			$ch = curl_init($url . '?' . $query);
+//			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//			curl_setopt($ch, CURLOPT_HEADER, 1);
+//			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+//			$response = curl_exec($ch);
+//			$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+//			$header = substr($response, 0, $header_size);
+//			$body = substr($response, $header_size);
+//			$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//			curl_close($ch);
+//
+//			$api_results = ["header" => $header, "body" => json_decode($body), "code" => $code];
+//			$json_results = json_encode($api_results);
+//
+//			// Remove cache file on error to avoid writing wrong xml
+//			if ( $api_results && $json_results )
+//				file_put_contents($cache_file, $json_results);
+//			else
+//				unlink( $cache_file);
+//		} else {
+//			$json_results = file_get_contents($cache_file);
+//		}
+//		return (array) json_decode($json_results);
 	}
 
 	public static function File( $url )
